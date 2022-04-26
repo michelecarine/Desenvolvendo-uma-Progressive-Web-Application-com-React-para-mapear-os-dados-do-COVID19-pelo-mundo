6dae3069821abc4e9a80e6030a574bc24af08948
Michele Carine <102440706+michelecarine@users.noreply.github.com>
2022-04-25 21:36:37 -0300

    class finished

---
+++ scr/setupTests.mk
@@ -0,0 +1,5 @@
+// jest-dom adiciona matchers personalizados de jest para declaração em nós DOM.
+// permite que você faça coisas como:
+// expect(element).toHaveTextContent(/react/i)
+// saiba mais: https://github.com/testing-library/jest-dom
+import  '@testing-library/jest-dom/extend-expect' ;
